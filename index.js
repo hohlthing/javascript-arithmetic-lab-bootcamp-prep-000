52fabc9e57a1f88ec6980239c769ecb18b4a8b3a
michael hohl <mhohl@rocketmail.com>
2018-03-08 19:05:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,29 +17,8 @@ function dec(n) {
   return n - 1
 }
 function makeInt(n) {
-  par
+  return parseInt(n,10)
 }
 function preserveDecimal(n) {
-  
+  return parseFloat(n)
 }
-
-/*  7) makeInt(n) parses n as an integer and returns the parsed integer:
-     ReferenceError: makeInt is not defined
-      at Context.<anonymous> (test/index-test.js:34:5)
-
-  8) makeInt(n) assumes base 10:
-     ReferenceError: makeInt is not defined
-      at Context.<anonymous> (test/index-test.js:38:5)
-
-  9) makeInt(n) returns NaN as appropriate:
-     ReferenceError: makeInt is not defined
-      at Context.<anonymous> (test/index-test.js:42:5)
-
-  10) preserveDecimal(n) preserves n's decimals (it parses n as a floating point number) and returns the parsed number:
-     ReferenceError: preserveDecimal is not defined
-      at Context.<anonymous> (test/index-test.js:48:5)
-
-  11) preserveDecimal(n) returns NaN as appropriate:
-     ReferenceError: preserveDecimal is not defined
-      at Context.<anonymous> (test/index-test.js:52:5)
-      */
